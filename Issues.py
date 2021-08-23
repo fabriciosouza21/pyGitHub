8decbc8bc6756e34f64e0ea5dfc1162464ccc919
unknown <fabriciosouza21777@gmail.com>
2021-08-23 15:27:02 -0300

    criado o metodo empty que impedi que os atributos author, title e body tenha valores None

--- Issues.py
+++ Issues.py
@@ -1,14 +1,19 @@
 class Issues:
     def __init__(self, number,author,title,body):
         self.number = number
-        self.author = author
-        self.title = title
-        self.body = body
+        self.author = self.empty(author)
+        self.title = self.empty(title)
+        self.body = self.empty(body)
         self.comments = []
     
     def add_comment(self,comment):
         self.comments.append(comment)
     
+    def empty(self,text):
+        if not text:
+            return " "
+        return text
+
     def to_dict(self):
         convert = {}
         convert["number"] = self.number
@@ -18,6 +23,7 @@ class Issues:
         for comment in self.comments:
             convert["comments"].append(comment.to_string())
         return convert         
+
 
     def __str__(self) -> str:       
         return str(self.number)+ " " + self.author + " " + self.title
