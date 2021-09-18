becca988e6418bd45632fde6fa886e5623a1a5eb
fabricio21777 <fabriciosouza21777@gmail.com>
2021-09-18 11:10:01 -0300

    teste inicial realizado

--- watson-tone-analyzer/ibmWatsonRequest.py
+++ watson-tone-analyzer/ibmWatsonRequest.py
@@ -1,14 +1,15 @@
 import json
 from ibm_watson import ToneAnalyzerV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from LOCAL_VARIABLES import apiKey,url
 
-authenticator = IAMAuthenticator('{apikey}')
+authenticator = IAMAuthenticator(apikey=apiKey)
 tone_analyzer = ToneAnalyzerV3(
     version='2017-09-21',
     authenticator=authenticator
 )
 
-tone_analyzer.set_service_url('{url}')
+tone_analyzer.set_service_url(service_url=url)
 
 text = 'Team, I know that times are tough! Product '\
     'sales have been disappointing for the past three '\
