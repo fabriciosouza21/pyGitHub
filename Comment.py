1af21f7d2e804f9bf4c981d80e8dc90f8b6f7dd8
fabricio21777 <fabriciosouza21777@gmail.com>
2021-09-22 13:12:10 -0300

    obtendo as issues apartir das paginas

--- Comment.py
+++ Comment.py
@@ -1,15 +1,19 @@
 class Comment:
-    def __init__(self, id,author,body):
+    def __init__(self, id, author, body=""):
         self.id = id
-        self.author = author
-        self.body = body
-    
+        if not body:
+            self.body= ""
+            self.author = author
+        else:
+            self.author = author
+            self.body = body
+
     def to_dict(self):
         convert = {}
         convert["id"] = self.id
         convert["author"] = self.author
         convert["body"] = self.body
-        return convert         
+        return convert
 
-    def __str__(self) -> str:       
-        return str(self.id)+ " " + self.author
+    def __str__(self) -> str:
+        return str(self.id) + " " + self.author
--- getIssues.py
+++ getIssues.py
@@ -1,3 +1,4 @@
+import json
 from util.Issues_whith_more_comments import Issues_whith_more_comments
 from util.Remove_single_crase import Remove_single_crase
 from util.Remove_url import Remove_url
@@ -6,13 +7,31 @@ from util.Process_text import Process_text
 from github import Github
 from Issues import Issues
 from Comment import Comment
+from LOCAL_VARIABLE import token
 
-token = "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"
+
+def writeCommentsJson(comments):
+    commentsjson = {}
+    commentslist = []
+    for comment in comments:
+        commentslist.append(comment.to_dict())
+    commentsjson["comments"] = commentslist
+
+    with open('comments.json', 'a', encoding='utf16') as f:
+        json.dump(commentsjson, f, ensure_ascii=False,
+                  sort_keys=True, indent=2, separators=(',', ':'))
+
+
+def last_page(issues):
+    last_page_index = issues._getLastPageUrl().find("page")
+    pages = issues._getLastPageUrl()
+    _, last_page = pages[last_page_index:].split("=")
+    return int(last_page)
+
 g = Github(token)
 repo = g.get_repo("spring-projects/spring-boot")
 issues = repo.get_issues(state="all")
-issues_all = []
-i = 0
+all_comments = []
 process_text = Process_text()
 remove_three_crase = Remove_three_crase()
 remove_url = Remove_url()
@@ -21,34 +40,20 @@ process_text.add_cleaner(remove_three_crase)
 process_text.add_cleaner(remove_url)
 process_text.add_cleaner(remove_single_crase)
 
-for issue in issues:
-    if i>=4900:
-        break
-    if not issue.body:
-        custom_issue = Issues(issue.number,issue.user.name,issue.title,process_text.run_cleaner("") )
-    else:
-         custom_issue = Issues(issue.number,issue.user.name,issue.title,process_text.run_cleaner(issue.body) )
-    
+last_page = last_page(issues)
+print(last_page)
+pages = issues.get_page(0)
+'''
+for issue in pages:
+    comment = Comment(issue.number, issue.user.name, issue.body)
+    comment.body = process_text.run_cleaner(comment.body)
+    all_comments.append(comment)
     comments = issue.get_comments()
     for comment in comments:
-        custom_comment = Comment(comment.id,comment.user.name,process_text.run_cleaner(comment.body))
-        custom_issue.add_comment(custom_comment)
-  
-    issues_all.append(custom_issue)
-    i+=1
+        comment = Comment(issue.number, comment.user.name,comment.body)
+        comment.body = process_text.run_cleaner(comment.body)
+        all_comments.append(comment)
+   
+writeCommentsJson(all_comments)
 
-issuses_whith_more_comments =  Issues_whith_more_comments(4)
-top_comments = issuses_whith_more_comments.get_issues(issues_all)
-
-for issue in top_comments:
-    print (issue.number)
-
-print(len(top_comments))
-
-'''x = ["--->"+issue.title + "\n" + issue.body for issue in issues_all]
-with open('issues.txt', 'w',encoding='utf-8') as arquivo:
-    arquivo.writelines("\n---> nova linha\n".join(x))'''
-
-
-
-
+'''
