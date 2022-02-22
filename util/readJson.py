bb59015249f9f66445dbce6972b9977621d124af
Fabricio Miranda <fabricio@itexto.com.br>
2022-02-22 13:38:54 -0300

    buscar todas as issues e comentarios repositorio

--- util/readJson.py
+++ util/readJson.py
@@ -4,7 +4,7 @@ import os
 def readJson(fileName,path="database"):
     arquivo = os.path.exists(f'{path}/{fileName}.json')
     if arquivo:
-        with open(f'{path}/{fileName}.json', 'r',encoding="utf-16") as f:
+        with open(f'{path}/{fileName}.json', 'r',encoding="utf-8") as f:
             return json.load(f)
     else:
         return {}
--- util/writeCommentsJsonSomente.py
+++ util/writeCommentsJsonSomente.py
@@ -1,5 +1,5 @@
 import json
 def writeCommentsJson(comments,nameFile="file"):
-    with open(f'database/{nameFile}.json', 'w', encoding='utf16') as f:
+    with open(f'database/{nameFile}.json', 'w', encoding='utf-8') as f:
         json.dump(comments, f, ensure_ascii=False,
                   sort_keys=True, indent=2, separators=(',', ':'))
