e0fbea0bcf797da48018f2e6ad92a0e015990230
Fabricio Miranda <fabricio@itexto.com.br>
2022-03-22 09:12:15 -0300

    ajuste basicos

--- getIssue.py
+++ getIssue.py
@@ -4,31 +4,8 @@ from entities.Comment import Comment
 from util.resquest.GitApiHearderResponse import GitApiHearderResponse
 from util.writeCommentsJsonSomente import writeCommentsJson
 from util.readJson import readJson
-from github import Github
 import os
 import requests
-
-
-def InstantiateComments(issue):
-    all_comments = []
-    comment = Comment(issue.number, issue.user.name,
-                      issue.created_at, issue.body)
-    all_comments.append(comment)
-    comments = issue.get_comments()
-    for comment in comments:
-        custom_comment = Comment(
-            issue.number, comment.user.name, comment.created_at, comment.body)
-        all_comments.append(custom_comment)
-    return all_comments
-
-
-def get_issue():
-    token = ''
-    g = Github(token)
-    repo = g.get_repo("spring-projects/spring-boot")
-    issues = repo.get_issue(10907)
-    comments = InstantiateComments(issues)
-    writeCommentsJson(comments,"")  
 
 
 def instantiate_comments_dict(issue):
--- util/writeCommentsJsonSomente.py
+++ util/writeCommentsJsonSomente.py
@@ -2,11 +2,6 @@ import json
 
 
 def writeCommentsJson(comments, nameFile="file"):
-
-    commentsjson = {}
-
-    commentsjson["comments"] = comments
-
     with open(f'database/{nameFile}.json', 'w', encoding='utf-8') as f:
         json.dump(comments, f, ensure_ascii=True,
 
