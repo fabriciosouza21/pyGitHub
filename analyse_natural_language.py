4de6ce56da37c8c2676530f75e57c3153c218b03
Fabricio Miranda <fabricio@itexto.com.br>
2022-07-06 08:55:40 -0300

    comentarios somente de autores toxicos

--- analyse_natural_language.py
+++ analyse_natural_language.py
@@ -15,7 +15,7 @@ def get_comments(comment):
 
 comentarios_total = []    
 def analyse_natural_language():
-    pasta = './database-natural-language'
+    pasta = './database/issues'
     arquivos_local = []
     for diretorio, subpastas, arquivos in os.walk(pasta):
         for arquivo in arquivos:
@@ -28,30 +28,45 @@ def analyse_natural_language():
         request_ibm_watson_natural_language_understanding(repo=issue["repository"], text= text)
 
 def analyse_natural_language_comments():
-    pasta = './database-natural-language'
+    autores_toxicos_dict = readJson("autores_toxico", f"database/autores")
+    autores_toxicos = list(autores_toxicos_dict["autores_toxico"])
+    comments_qtd = 0
+    pasta = './database/issues'
     arquivos_local = []
     for diretorio, subpastas, arquivos in os.walk(pasta):
         for arquivo in arquivos:
             nome_arquivo, extensao = os.path.splitext(arquivo)
             arquivos_local.append(nome_arquivo)
+    
+    arquivos_local = sorted(arquivos_local)
     for arquivo in arquivos_local:
-        issue = readJson(arquivo, f"database-natural-language/issues/{arquivo}")
+        projeto = arquivo.split("-")[:-1]
+        projeto = "-".join(projeto)
+        
+        issue = readJson(arquivo, f"database/issues/{projeto}")
+        
         comments = issue["comments"]
         repo_name = issue["repository"]
         for comment in comments:
-            number_comment = 1
-            text =  comment["comment"]
-            user = comment["user"]
-            if(len(text) > 50):
-                if not os.path.isdir(f"resultWatson/comments/{repo_name}"):
-                    os.mkdir(f"resultWatson/comments/{repo_name}")
-                if not os.path.isdir(f"resultWatson/comments/{repo_name}/{user}"):
-                    os.mkdir(f"resultWatson/comments/{repo_name}/{user}")
-                response = request_ibm_watson_natural_language_understanding(repo=repo_name, text= text)
-                if(os.path.isfile(f"resultWatson/comments/{repo_name}/{user}/{user}_{repo_name}_{number_comment}.json")):
-                    number_comment += 1
-                writeDictToJson(response,f"{user}_{repo_name}_{number_comment}",f"resultWatson/comments/{repo_name}/{user}/")
-
+            if(comment["user"] in autores_toxicos):
+                number_comment = 1
+                text =  comment["comment"]
+                user = comment["user"]
+                comments_qtd+=1
+                # number_comment = save_result(text, repo_name, user, number_comment)     
+                
+    print(comments_qtd)
+def save_result(text, repo_name, user, number_comment):
+    if(len(text) > 50):
+        if not os.path.isdir(f"resultWatson/comments/{repo_name}"):
+            os.mkdir(f"resultWatson/comments/{repo_name}")
+        if not os.path.isdir(f"resultWatson/comments/{repo_name}/{user}"):
+            os.mkdir(f"resultWatson/comments/{repo_name}/{user}")
+        response = request_ibm_watson_natural_language_understanding(repo=repo_name, text= text)
+        if(os.path.isfile(f"resultWatson/comments/{repo_name}/{user}/{user}_{repo_name}_{number_comment}.json")):
+            number_comment += 1
+        writeDictToJson(response,f"{user}_{repo_name}_{number_comment}",f"resultWatson/comments/{repo_name}/{user}/")
+        return number_comment
 
 if __name__ == '__main__':
     analyse_natural_language_comments()
